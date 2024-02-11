f6953edf8690e68373ef1aae6c2ccb5bff69958c
GastonOrquera <gastonorquera29@gmail.com>
2024-02-11 04:43:13 -0300

    Prueba

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll('nav a');
 
 hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('active');
     nav.classList.toggle('active');
 
     navLinks.forEach(function (link) {
         link.addEventListener('click', function () {
+            hamburger.classList.remove('active');
             nav.classList.remove('active');
         });
     });
